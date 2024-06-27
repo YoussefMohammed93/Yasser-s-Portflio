1bfc3fb598178813e68e0eef6f85d6a5f9255d91
Youssef <ixyoussef46@gmail.com>
2024-06-27 04:05:06 +0300

    section one in home test

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,42 @@
+import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import SettingsBox from "./components/SettingsBox";
+import Circles from "./components/CirclesAnimation";
+import YasserImg from "/src/assets/yasser.jpg";
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
       <SettingsBox />
+      <Circles />
+      <main className="home-main p-main pt-20 lg:pt-44 bg-[var(--bg-main)] min-h-screen">
+        <div className="home-container grid grid-cols-1 lg:grid-cols-12 items-center">
+          <div className="col-span-1 lg:col-span-6 text-center lg:text-start mt-12">
+            <h1 className="text-4xl lg:text-5xl font-black text-[var(--text-color)]">
+              Hi I'm Yasser Galal
+            </h1>
+            <h2 className="text-lg lg:text-xl font-semibold mt-4 lg:mt-6 text-[var(--gray-color)]">
+              A Content Creator From Egypt
+            </h2>
+            <div className="mt-8 lg:mt-10 pb-8 lg:pb-12">
+              <Link
+                to="/"
+                className="text-white text-lg lg:text-xl bg-[var(--main-color)] font-bold py-1 px-8 lg:px-12 rounded-full border-2 border-transparent hover:border-[var(--text-color)] transition-all duration-500"
+              >
+                Projects
+              </Link>
+            </div>
+          </div>
+          <div className="col-span-1 lg:col-span-6 mt-8 lg:mt-0">
+            <img
+              src={YasserImg}
+              alt="Yasser Galal"
+              className="w-full h-auto relative z-20 rounded-3xl"
+            />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/CirclesAnimation.jsx
@@ -0,0 +1,31 @@
+export default function Circles() {
+  return (
+    <div className="circles">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
